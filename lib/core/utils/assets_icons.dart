acf7ef13a095ef8641ba945dead1a52b7bbff43d
Essam <essam.ibrahim.0062@gmail.com>
2026-04-22 21:11:26 +0200

    Add asset images and icons for onboarding and app functionality
    - Added onboarding image 3 to the assets/images directory.
    - Created assets_icons.dart to manage icon paths including tasky logo, logout, flag, send, timer, and trash icons.
    - Created assets_images.dart to manage image paths including onboarding images and home image.

---
+++ lib/core/utils/assets_icons.dart
@@ -0,0 +1,8 @@
+class AppIcon {
+  static String taskyLogo = "assets/icons/tasky_logo.png";
+  static String logoutIcon = "assets/icons/logout.png";
+  static String flagIcon = "assets/icons/flag.png";
+  static String sendIcon = "assets/icons/send.png";
+  static String timerIcon = "assets/icons/timer.png";
+  static String trashIcon = "assets/icons/trash.png";
+}
---
+++ lib/core/utils/assets_images.dart
@@ -0,0 +1,6 @@
+class AppImage {
+  static String onbordingImage1 = "assets/images/onbording_image_1.png";
+  static String onbordingImage2 = "assets/images/onbording_image_2.png";
+  static String onbordingImage3 = "assets/images/onbording_image_3.png";
+  static String homeImage = "assets/images/home_image.png";
+}
