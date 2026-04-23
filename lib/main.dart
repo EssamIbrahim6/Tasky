b1b5573365afae3fa3ca7ae92f24ba2e9ba61dc9
Essam <essam.ibrahim.0062@gmail.com>
2026-04-23 23:50:30 +0200

    Add missing newline for code readability in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:tasky/core/utils/assets_icons.dart';
 import 'package:tasky/core/utils/assets_images.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 await Firebase.initializeApp(
